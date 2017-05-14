5d61409b9ad0262ca549a87bd42bc9410975a5e3
Ryan Donnelly <ryanjdonnelly@gmail.com>
2017-05-14 00:48:06 -0400

    update sorting on character screen

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -2,10 +2,39 @@ import Immutable from 'immutable';
 
 import * as Destiny from '../lib/Destiny';
 
+const affiliationRank = {
+  villain: 0,
+  hero: 1,
+};
+
+const factionRank = {
+  red: 0,
+  blue: 1,
+  yellow: 2,
+};
 
 const initialState = Immutable.fromJS({
   cards: Destiny.cards
     .filter(card => card.get('type') === 'character')
+    .sort((a, b) => {
+      if (b.get('affiliation') !== a.get('affiliation')) {
+        return affiliationRank[b.get('affiliation')] < affiliationRank[a.get('affiliation')] ? 1 : -1;
+      }
+
+      if (b.get('faction') !== a.get('faction')) {
+        return factionRank[b.get('faction')] < factionRank[a.get('faction')] ? 1 : -1;
+      }
+
+      if (b.get('isUnique') !== a.get('isUnique')) {
+        return b.get('isUnique') > a.get('isUnique') ? 1 : -1;
+      }
+
+      if (b.get('name') !== a.get('name')) {
+        return b.get('name') < a.get('name') ? 1 : -1;
+      }
+
+      return 0;
+    })
     .map(card => (Immutable.fromJS({
       id: card.get('id'),
       name: card.get('name'),
