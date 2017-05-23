ba7ec8f4c570256ffb20acdd12b27e92a3456692
Ryan Donnelly <ryanjdonnelly@gmail.com>
2017-05-23 18:02:12 -0400

    add bugsnag

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,11 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 
 import setup from './src/setup';
+
+// eslint-disable-next-line no-unused-vars
+const bugsnag = new Client();
 
 AppRegistry.registerComponent('SWDTeamBuilder', setup);
