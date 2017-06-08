5e9e846640fca401116ddcc9c024ea2ec734f6da
Ryan Donnelly <ryanjdonnelly@gmail.com>
2017-06-07 23:15:25 -0400

    disable character buttons when selected

--- src/components/Characters/views/CharactersDetailsScreen.js
+++ src/components/Characters/views/CharactersDetailsScreen.js
@@ -18,10 +18,41 @@ import SWDIcon from '../../SWDIcon/SWDIcon';
 const styles = StyleSheet.create({
   buttonView: {
     alignItems: 'center',
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    marginTop: 16,
-    width: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    borderRadius: 4,
+    flex: 1,
+    flexDirection: 'row',
+    height: 42,
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  buttonDisabled: {
+    backgroundColor: 'rgba(155, 89, 182, 0.2)',
+  },
+  buttonGreen: {
+    backgroundColor: 'rgba(46, 204, 113, 1.0)',
+  },
+  buttonOrange: {
+    backgroundColor: 'rgba(230, 126, 34, 1.0)',
+  },
+  buttonPurple: {
+    backgroundColor: 'rgba(155, 89, 182, 1.0)',
+  },
+  buttonIcon: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '600',
+    textAlign: 'center',
   },
 });
 
@@ -38,103 +69,72 @@ class CharactersDetailsScreen extends React.Component {
     const { deckState } = this.props;
 
     const characterIsInDeck = deckState.get('cards').some(deckCard => deckCard.get('id') === card.get('id'));
+    const deckCharacter = deckState.get('cards').find(deckCard => deckCard.get('id') === card.get('id'));
 
-    const addButton = !characterIsInDeck ?
+    const addButton = !characterIsInDeck || !card.get('isUnique') ?
       <TouchableOpacity
         onPress={ () => this.props.addCharacter(card) }
-        style={{
-          backgroundColor: 'rgba(46, 204, 113, 1.0)',
-          borderRadius: 4,
-          flex: 1,
-          padding: 16,
-        }}>
-        <Text style={{ color: 'white', fontSize: 16, fontWeight: '600', textAlign: 'center' }}>{ 'Add Character' }</Text>
+        style={ [styles.button, styles.buttonGreen] }
+      >
+        <Text style={ styles.buttonText }>{ 'Add Character' }</Text>
       </TouchableOpacity> : null;
 
     const removeButton = characterIsInDeck ?
       <TouchableOpacity
         onPress={ () => this.props.removeCharacter(card) }
-        style={{
-          backgroundColor: 'rgba(230, 126, 34, 1.0)',
-          borderRadius: 4,
-          flex: 1,
-          padding: 16,
-        }}>
-        <Text style={{ color: 'white', fontSize: 16, fontWeight: '600', textAlign: 'center' }}>{ 'Remove Character' }</Text>
+        style={ [styles.button, styles.buttonOrange] }
+      >
+        <Text style={ styles.buttonText }>{ 'Remove Character' }</Text>
       </TouchableOpacity> : null;
 
     const anyButton = characterIsInDeck && card.get('isUnique') ?
       <TouchableOpacity
+        disabled={ deckCharacter.get('isElite') === null }
         onPress={ () => this.props.setCharacterAny(card) }
-        style={{
-          alignItems: 'center',
-          backgroundColor: 'rgba(155, 89, 182, 1.0)',
-          borderRadius: 4,
-          flex: 1,
-          flexDirection: 'row',
-          height: 42,
-          justifyContent: 'center',
-          marginRight: 4,
-        }}>
-        <Text style={{ color: 'white', fontSize: 14, fontWeight: '600', textAlign: 'center' }}>{ 'Any' }</Text>
+        style={ [styles.button, styles.buttonPurple, { marginRight: 4 }, deckCharacter.get('isElite') === null && styles.buttonDisabled] }
+      >
+        <Text style={ styles.buttonText }>{ 'Any' }</Text>
       </TouchableOpacity> : null;
 
     const regularButton = characterIsInDeck && card.get('isUnique') ?
       <TouchableOpacity
+        disabled={ deckCharacter.get('isElite') === false }
         onPress={ () => this.props.setCharacterRegular(card) }
-        style={{
-          alignItems: 'center',
-          backgroundColor: 'rgba(155, 89, 182, 1.0)',
-          borderRadius: 4,
-          flex: 1,
-          flexDirection: 'row',
-          height: 42,
-          justifyContent: 'center',
-          marginHorizontal: 4,
-        }}>
-        <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={{ fontSize: 16, color: 'white', textAlign: 'center' }} />
+        style={ [styles.button, styles.buttonPurple, { marginHorizontal: 4 }, deckCharacter.get('isElite') === false && styles.buttonDisabled] }
+      >
+        <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={ styles.buttonIcon } />
       </TouchableOpacity> : null;
 
     const eliteButton = characterIsInDeck && card.get('isUnique') && card.get('pointsElite') ?
       <TouchableOpacity
+        disabled={ deckCharacter.get('isElite') === true }
         onPress={ () => this.props.setCharacterElite(card) }
-        style={{
-          alignItems: 'center',
-          backgroundColor: 'rgba(155, 89, 182, 1.0)',
-          borderRadius: 4,
-          flex: 1,
-          flexDirection: 'row',
-          height: 42,
-          justifyContent: 'center',
-          marginLeft: 4,
-        }}>
+        style={ [styles.button, styles.buttonPurple, { marginLeft: 4 }, deckCharacter.get('isElite') === true && styles.buttonDisabled] }
+      >
         <View>
-          <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={{ fontSize: 16, color: 'white', textAlign: 'center' }} />
+          <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={ styles.buttonIcon } />
         </View>
         <View style={{ marginLeft: 8 }}>
-          <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={{ fontSize: 16, color: 'white', textAlign: 'center' }} />
+          <SWDIcon type={ 'DIE' } font={ 'swdestiny' } style={ styles.buttonIcon } />
         </View>
       </TouchableOpacity> : null;
 
     return (
       <View style={{
-        flex: 1,
-        backgroundColor: 'white',
         alignItems: 'center',
+        backgroundColor: 'white',
+        flex: 1,
         justifyContent: 'center',
       }}>
         <ScrollView style={{
-          flex: 1,
-          width: '100%',
           alignContent: 'center',
+          flex: 1,
           paddingHorizontal: 16,
+          width: '100%',
         }}>
-          <View style={{ flex: 1, paddingTop: 16, width: '100%', height: 300 }}>
+          <View style={{ flex: 1, height: 300, paddingVertical: 16 }}>
             <Image
-              style={{
-                flex: 1,
-                width: '100%',
-              }}
+              style={{ flex: 1 }}
               resizeMode='contain'
               source={ cardImages.get(card.get('id')) }
             />
@@ -146,6 +146,8 @@ class CharactersDetailsScreen extends React.Component {
           </View>
           <View style={ styles.buttonView }>
             { addButton }
+          </View>
+          <View style={ styles.buttonView }>
             { removeButton }
           </View>
         </ScrollView>
@@ -153,6 +155,17 @@ class CharactersDetailsScreen extends React.Component {
     );
   }
 }
+
+CharactersDetailsScreen.propTypes = {
+  navigation: React.PropTypes.object.isRequired,
+  deckState: React.PropTypes.object.isRequired,
+
+  addCharacter: React.PropTypes.func.isRequired,
+  setCharacterAny: React.PropTypes.func.isRequired,
+  setCharacterElite: React.PropTypes.func.isRequired,
+  setCharacterRegular: React.PropTypes.func.isRequired,
+  removeCharacter: React.PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({ deckState: state.deckReducer });
 
