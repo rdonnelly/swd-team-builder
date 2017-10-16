82d9290f511cdb005754f5a09cd0baa8cd5b3016
Ryan Donnelly <ryanjdonnelly@gmail.com>
2017-10-16 19:32:21 -0400

    use more efficient update in immutable

--- src/reducers/deckReducer.js
+++ src/reducers/deckReducer.js
@@ -12,9 +12,7 @@ const deckReducer = (state = initialState, action) => {
         .findIndex(characterObject => characterObject.get('id') === action.payload.characterObject.get('id'));
 
       if (existingCardIndex !== -1) {
-        return state.update('characters', characterObjects =>
-          characterObjects.update(existingCardIndex, characterObject =>
-            characterObject.update('count', count => count + 1)));
+        return state.updateIn(['characters', existingCardIndex, 'count'], count => count + 1);
       }
 
       const characterObject = Immutable.fromJS({
@@ -31,27 +29,21 @@ const deckReducer = (state = initialState, action) => {
       const existingCardIndex = state.get('characters')
         .findIndex(characterObject => characterObject.get('id') === action.payload.characterObject.get('id'));
 
-      return state.update('characters', characterObjects =>
-        characterObjects.update(existingCardIndex, characterObject =>
-          characterObject.set('numDice', 0)));
+      return state.setIn(['characters', existingCardIndex, 'numDice'], 0);
     }
 
     case 'SET_CHARACTER_REGULAR_IN_DECK': {
       const existingCardIndex = state.get('characters')
         .findIndex(characterObject => characterObject.get('id') === action.payload.characterObject.get('id'));
 
-      return state.update('characters', characterObjects =>
-        characterObjects.update(existingCardIndex, characterObject =>
-          characterObject.set('numDice', 1)));
+      return state.setIn(['characters', existingCardIndex, 'numDice'], 1);
     }
 
     case 'SET_CHARACTER_ELITE_IN_DECK': {
       const existingCardIndex = state.get('characters')
         .findIndex(characterObject => characterObject.get('id') === action.payload.characterObject.get('id'));
 
-      return state.update('characters', characterObjects =>
-        characterObjects.update(existingCardIndex, characterObject =>
-          characterObject.set('numDice', 2)));
+      return state.setIn(['characters', existingCardIndex, 'numDice'], 2);
     }
 
     case 'REMOVE_CHARACTER_FROM_DECK': {
@@ -65,8 +57,7 @@ const deckReducer = (state = initialState, action) => {
       return state.update('characters', (characterObjects) => {
         const existingDeckCharacterObject = characterObjects.get(existingCardIndex);
         if (existingDeckCharacterObject.get('count') > 1) {
-          return characterObjects.update(existingCardIndex, characterObject =>
-            characterObject.update('count', count => count - 1));
+          return characterObjects.updateIn([existingCardIndex, 'count'], count => count - 1);
         }
 
         return characterObjects.delete(existingCardIndex);
