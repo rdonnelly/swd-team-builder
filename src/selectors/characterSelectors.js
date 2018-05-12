25f3395f13f0a85f9bca2dcb1e4cb676e3765e90
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-05-11 21:33:14 -0400

    do not reorder characters

--- src/selectors/characterSelectors.js
+++ src/selectors/characterSelectors.js
@@ -9,16 +9,5 @@ export const getCharacters = createSelector(
 
 export const getCharactersSorted = createSelector(
   [getCharacters],
-  characters =>
-    characters.sort((a, b) => {
-      if (!a.isIncompatible && b.isIncompatible) {
-        return -1;
-      }
-
-      if (a.isIncompatible && !b.isIncompatible) {
-        return 1;
-      }
-
-      return a.rank - b.rank;
-    }),
+  characters => characters.sort((a, b) => a.rank - b.rank),
 );
