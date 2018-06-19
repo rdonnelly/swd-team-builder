bb6ee5a5b7d8f6a5696565d3f45627e6d135e6ac
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-06-19 09:30:38 -0400

    clean up

--- index.ios.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { Client } from 'bugsnag-react-native';
-
-import setup from './src/setup';
-
-// eslint-disable-next-line no-unused-vars
-const bugsnag = new Client();
-
-AppRegistry.registerComponent('SWDTeamBuilder', setup);
