6da3275718cf86034898cfc0e6a67fdb8e02aa5c
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-06-24 20:26:49 -0400

    random cleanup

--- ios/SWDTeamBuilder/AppDelegate.m
+++ ios/SWDTeamBuilder/AppDelegate.m
@@ -20,46 +20,46 @@
 
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"SWDTeamBuilder"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:236.0/255.0 green:240.0/255.0 blue:241.0/255.0 alpha:1.0];
-  
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-  
-  
+
+
+
   UIView *loadingView = [[UIView alloc] initWithFrame:rootView.bounds];
   [loadingView setBackgroundColor:[[UIColor alloc] initWithRed:236.0/255.0 green:240.0/255.0 blue:241.0/255.0 alpha:1.0]];
-  
-  
-  
+
+
+
   UIImageView* splashView = [[UIImageView alloc] initWithFrame:loadingView.bounds];
   [splashView setImage:[UIImage imageNamed:@"splash"]];
   [splashView setContentMode:UIViewContentModeCenter];
   [loadingView addSubview:splashView];
-  
-  
-  
+
+
+
   UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
   spinner.color = [[UIColor alloc] initWithRed:155.0/255.0 green:89.0/255.0 blue:182.0/255.0 alpha:1.0];
   spinner.center = CGPointMake(loadingView.frame.size.width * 0.5, loadingView.frame.size.height * 0.85);
   [spinner startAnimating];
   [loadingView addSubview:spinner];
-  
-  
-  
+
+
+
   rootView.loadingView = loadingView;
   rootView.loadingViewFadeDelay = 0.5;
   rootView.loadingViewFadeDuration = 0.5;
-  
+
   return YES;
 }
 
