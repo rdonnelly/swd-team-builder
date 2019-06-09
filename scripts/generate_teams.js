064a404c8ccfd307a0f201f56233652632453d5f
Ryan Donnelly <ryanjdonnelly@gmail.com>
2019-06-09 15:53:59 -0400

    update data to include new balance
    Former-commit-id: a13e534e84ad38f94d5aa4ef75ce99eff02f1e97

--- scripts/generate_teams.js
+++ scripts/generate_teams.js
@@ -557,8 +557,9 @@ async function save() {
 (async function run() {
   await open();
 
-  const numFormats = formats.length;
-  formats.forEach(async (format, index) => {
+  const elibibleFormats = formats.filter((format) => format.code !== 'INF');
+  const numFormats = elibibleFormats.length;
+  elibibleFormats.forEach(async (format, index) => {
     console.log('\x1b[34m%s\x1b[0m', `Generating teams for ${format.name} (${index + 1}/${numFormats})`);
     const numTeams = teams.length;
 
