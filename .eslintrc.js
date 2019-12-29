67543616bdeb0cec8230976e4776c44706fec0ad
Ryan Donnelly <ryanjdonnelly@gmail.com>
2019-12-29 14:34:13 -0500

    upgrade to react native 61

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,39 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-        jsx: true,
-        modules: true,
-    },
-  },
-  plugins: [
-    'import',
-    'react',
-    'react-native',
-  ],
-  extends: [
-    'airbnb-base',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:react-native/all',
-    'plugin:react/recommended',
-  ],
-  rules: {
-    'arrow-parens': ['error', 'always'],
-    'class-methods-use-this': ['off'],
-    'import/prefer-default-export': 'off',
-    'no-param-reassign': ['error', { 'props': false }],
-    'operator-linebreak': ['error', 'after'],
-    'react-native/sort-styles': 'off',
-  },
-  settings: {
-    react: {
-      version: '16.6.0',
-    },
-  },
+  root: true,
+  extends: '@react-native-community',
 };
+
+// module.exports = {
+//   parser: 'babel-eslint',
+//   parserOptions: {
+//     ecmaFeatures: {
+//         jsx: true,
+//         modules: true,
+//     },
+//   },
+//   plugins: [
+//     'import',
+//     'react',
+//     'react-native',
+//   ],
+//   extends: [
+//     'airbnb-base',
+//     'plugin:import/errors',
+//     'plugin:import/warnings',
+//     'plugin:react-native/all',
+//     'plugin:react/recommended',
+//   ],
+//   rules: {
+//     'arrow-parens': ['error', 'always'],
+//     'class-methods-use-this': ['off'],
+//     'import/prefer-default-export': 'off',
+//     'no-param-reassign': ['error', { 'props': false }],
+//     'operator-linebreak': ['error', 'after'],
+//     'react-native/sort-styles': 'off',
+//   },
+//   settings: {
+//     react: {
+//       version: '16.6.0',
+//     },
+//   },
+// };
